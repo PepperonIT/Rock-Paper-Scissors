72e1467ea11ab1fac8102cf45fe24e6cb3194955
SimonBerghem <bersim-8@student.ltu.se>
2022-11-10 14:32:50 +0000

    Added tracking and tts

--- src/app.py
+++ src/app.py
@@ -7,19 +7,18 @@ def main():
     session = qi.Session()
     session.connect("tcp://{0}:{1}".format("130.240.238.32", 9559))
 
-    motion_service = session.service("ALMotion")
-    tablet = session.service("ALTabletService")
-    camera_service = session.service("ALVideoDevice")
 
-    motions = Connection(motion_service, tablet, camera_service)
+    motions = Connection(session)
     camera = Camera(session)
 
-    camera.save_image_frame("test.jpg")
-    motions.capture_frame()
+    # camera.save_image_frame("test.jpg")
+    # motions.capture_frame()
+
+    motions.startTracking()
 
     gesture = motions.select_gesture()
     motions.shake_arm()
     motions.do_gesture(gesture)
 
-
+    motions.stopTracking()
 main()
