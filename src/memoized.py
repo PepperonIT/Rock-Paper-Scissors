69c20c8b70c3d43d18e9eb8ca8d65e417c06fe67
naddah-5 <andersoskardahl@hotmail.com>
2022-11-22 15:12:19 +0000

    wip

--- src/memoized.py
+++ src/memoized.py
@@ -2,20 +2,39 @@ import random
 import datetime
 
 
+def copy_exclude(list, exclude):
+    """
+    Takes a list, list[elemA], and a variable elemA, returns a copy of list with no 
+    instances of elemA.
+    """
+    result_list = []
+    for i in list:
+        if i != exclude:
+            result_list.append(i)
+    return result_list
+
+
 class Memoize(type):
+    """
+    Memoizing random function for RPS, returns an integer with one of three values, 0, 1, or 2.
+    """
     _instance = {}
-    memoization_limit: int = 10
-    memoization: list[float] = []
-    weighted_limits: dict[str, float] = {
-        "Rock": 1.0,
-        "Paper": 2.0,
-        "Scissors": 3.0
-    }
+    __game_values = [0, 1, 2]
+    __memoization_limit = 6
+    __memoization = []
 
     def __call__(cls):
         if cls not in cls._instance:
             cls._instance[cls] = super(Memoize, cls).__call__()
         return cls._instance[cls]
 
-    def memoize(self) -> int:
+    def memoize(self):
         random.seed(datetime.datetime.now())
+
+    def new_memo(self, outcome):
+        memoization = copy_exclude(self.__memoization, None)
+        addition = copy_exclude(self.__game_values, outcome)
+
+        if len(memoization) > self.__memoization_limit:
+            while len(memoization) > self.__memoization_limit:
+                self._memoization = self._memoization[1:]
