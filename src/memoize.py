b5f6ac42d6cfd7c7a224727a2d2fd5a614c397f1
naddah-5 <andersoskardahl@hotmail.com>
2022-11-24 14:39:18 +0000

    Memoize is now a singleton, unit tests wip

--- src/memoize.py
+++ src/memoize.py
@@ -2,41 +2,51 @@ import random
 import datetime
 
 
-@staticmethod
-def copy_exclude(list, exclude):
-    """
-    Takes a list, list[elemA], and a variable elemA, returns a copy of list with no 
-    instances of elemA.
-    """
-    result_list = []
-    for i in list:
-        if i != exclude:
-            result_list.append(i)
-    return result_list
+class Memoize():
 
-
-class Memoize(type):
     """
     Memoizing random function for RPS, returns an integer with one of three values, 0, 1, or 2.
     """
-    _instance = {}
-    __game_values = [0, 1, 2]
-    __memoization_limit = 6
-    __memoization = []
 
-    def __call__(cls):
-        if cls not in cls._instance:
-            cls._instance[cls] = super(Memoize, cls).__call__()
-        return cls._instance[cls]
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if Memoize.__instance == None:
+            Memoize()
+        return Memoize.__instance
+
+    def __init__(self):
+        if Memoize.__instance != None:
+            raise Exception("Singleton class initialized a second time.")
+        else:
+            Memoize.__instance = self
+            self.__game_values = [0, 1, 2]
+            self.__memoization_limit = 6
+            self.__memoization = []
+
+    @staticmethod
+    def copy_exclude(list, exclude):
+        # type: (list[int], int) -> list[int]
+        """
+        Takes a list, list[elemA], and a variable elemA, returns a copy of list with no 
+        instances of elemA.
+        """
+        result_list = []
+        for i in list:
+            if i != exclude:
+                result_list.append(i)
+        return result_list
 
     def memoize(self):
         random.seed(datetime.datetime.now())
 
     def update_memoization(self, outcome):
+        # type: (int) -> bool
         """
         Takes the new game outcome as an argument and updates the memoization based on the arguments value.
         """
-        memoize = copy_exclude(self.__game_values, outcome)
+        memoize = self.copy_exclude(self.__game_values, outcome)
         self.__memoization += memoize
 
         if len(self.__memoization) > self.__memoization_limit:
--- src/memoize_test.py
+++ src/memoize_test.py
@@ -5,7 +5,8 @@ from memoize import Memoize
 class TestMemoize(unittest.TestCase):
 
     def test_update_memoization_one_outcome(self):
-        result = Memoize().update_memoization(2)
+        memo = Memoize.getInstance()
+        result = Memoize.update_memoization(memo, 2)
         self.assertEqual(result, True, "Failed")
 
 
