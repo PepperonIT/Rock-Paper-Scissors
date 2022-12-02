f614d6569e749aa249810da3b927d3de2ca281e1
naddah-5 <andersoskardahl@hotmail.com>
2022-12-02 12:35:46 +0000

    type comments, type unit test for init method

--- src/game.py
+++ src/game.py
@@ -1,6 +1,7 @@
 class Game:
 
     def __init__(self, rounds=None):
+        # type: (int) -> None
         """
         Initiates the game, it is a best of 'x' rounds. Allows for different number of 
         rounds, but by default plays 3 rounds.
@@ -15,30 +16,35 @@ class Game:
         self.__human_score = 0
 
     def get_round_limit(self):
+        # type: () -> int
         """
         Returns the round limit for the game.
         """
         return self.__round_limit
 
     def get_computer_score(self):
+        # type: () -> int
         """
         Returns the computers score.
         """
         return self.__computer_score
 
     def get_human_score(self):
+        # type: () -> int
         """
         Returns the human players score.
         """
         return self.__human_score
 
     def get_current_round(self):
+        # type: () -> int
         """
         Return the current round of the best of 'x' game.
         """
         return self.__current_round
 
     def check_winner(self):
+        # type: () -> bool
         """
         Returns True if there is a winner, otherwise returns False.
         """
@@ -47,6 +53,7 @@ class Game:
         return False
 
     def get_winner(self):
+        # () -> str | None
         """
         Returns the winner, if there is any, or 'Draw' if it is a draw. 
         Also checks that there is in fact a winner.
@@ -61,6 +68,7 @@ class Game:
             return "Draw"
 
     def update_game(self, winner=str):
+        # type: (str) -> None
         """
         Updates the game state based on the stated winner and increments the current round.
         This can be extended to account for several winners if there are more than two players.
--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -6,11 +6,25 @@ class TestGame(unittest.TestCase):
     def test_game_rounds_defaulting(self):
         default_game = Game()
         custom_game = Game(10)
-        invalid_game = Game(-1)
+        invalid_game_int_negative = Game(-1)
+        invalid_game_int_zero = Game(0)
+        invalid_game_string = Game("1")
+        invalid_game_bool_true = Game(True)
+        invalid_game_bool_false = Game(False)
+        invalid_game_float_whole = Game(2.0)
+        invalid_game_float = Game(2.5)
+        invalid_game_float_negative = Game(-2.3)
 
         self.assertEqual(default_game.get_round_limit(), 3)
         self.assertEqual(custom_game.get_round_limit(), 10)
-        self.assertEqual(invalid_game.get_round_limit(), 3)
+        self.assertEqual(invalid_game_int_negative.get_round_limit(), 3)
+        self.assertEqual(invalid_game_int_zero.get_round_limit(), 3)
+        self.assertEqual(invalid_game_string.get_round_limit(), 3)
+        self.assertEqual(invalid_game_bool_true.get_round_limit(), 3)
+        self.assertEqual(invalid_game_bool_false.get_round_limit(), 3)
+        self.assertEqual(invalid_game_float_whole.get_round_limit(), 3)
+        self.assertEqual(invalid_game_float.get_round_limit(), 3)
+        self.assertEqual(invalid_game_float_negative.get_round_limit(), 3)
 
     def test_update_game(self):
         game = Game()
