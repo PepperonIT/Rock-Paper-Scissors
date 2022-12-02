da44694def3cb880f5d177f427362d88d6c27a0c
Rickard Bemm <12218241+Rickos99@users.noreply.github.com>
2022-12-02 16:41:34 +0100

    Fix argument error in main (#14)

--- src/app.py
+++ src/app.py
@@ -2,9 +2,10 @@ import qi
 from connection import Connection
 
 
-def main(session):
+def main():
+    session = qi.Session()
     session.connect("tcp://{0}:{1}".format("130.240.238.32", 9559))
-    motions = Connection(session)
+    motions = Connection(session, "Swedish")
     motions.run_game()
 
 if __name__ == "__main__":
