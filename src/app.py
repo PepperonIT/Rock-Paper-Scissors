ebec00507550c615f591893c4a43d741585250f3
Anders Dahl <andersoskardahl@hotmail.com>
2022-12-06 15:42:28 +0100

    cleaned the code a bit (#16)

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 import qi
+import sys
 from connection import Connection
 
 
-def main():
+def main(lang):
     session = qi.Session()
     session.connect("tcp://{0}:{1}".format("130.240.238.32", 9559))
-    motions = Connection(session)
+    motions = Connection(session, lang)
     motions.game_loop()
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
