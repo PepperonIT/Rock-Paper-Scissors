420b20fc4b42e0aa156969412e15593cd9273398
Rickard Bemm <12218241+Rickos99@users.noreply.github.com>
2022-12-09 16:24:22 +0100

    Add image of human gesture to scoreboard (#19)

--- src/ai_rest.py
+++ src/ai_rest.py
@@ -10,9 +10,11 @@ def predict_on_images(images):
 
     Returns
     -------
-    int:
-        Returns the gesture id. If no gesture is found, returns -1. If no 
-        hand was found, returns -2. If an error occured, returns -3.
+    tuple(gesture:int, image_url:str):
+        Returns the gesture as an ``int``. If no gesture is found, returns -1. If no 
+        hand was found, returns -2. If an error occured, returns -3. 
+
+        The ``image_url`` is the url to the image that was generated on the server.
     """
     # Prepare request
     serialized_images = []
@@ -40,13 +42,16 @@ def predict_on_images(images):
 
     # Do something with the reponse
     print("response: {}".format(response_json))
+
+    gesture_image_url = response_json.get("images", None)
+    gesture_image_url = gesture_image_url.get("processed", "") if gesture_image_url is not None else ""
     if response_json["prediction"] == "ROCK":
-        return 0
+        return 0, gesture_image_url
     elif response_json["prediction"] == "PAPER":
-        return 1
+        return 1, gesture_image_url
     elif response_json["prediction"] == "SCISSORS":
-        return 2
+        return 2, gesture_image_url
     elif response_json["prediction"] == "NOTHING":
-        return -1
+        return -1, gesture_image_url
     else:
-        return -2
+        return -2, gesture_image_url
